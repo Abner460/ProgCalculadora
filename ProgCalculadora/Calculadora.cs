cce2e66e3c5c9f0332ccba986225be46883353ce
Abner Casteliano <abnercasteliano@outlook.com>
2020-03-29 22:54:30 -0300

    inicio prj

---
+++ ProgCalculadora/Calculadora.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgCalculadora
+{
+    public class Calculadora
+    {
+        private List<double> listNumero = new List<double>();
+
+        public void addNumero(double num)
+        {
+            this.listNumero.Add(num);
+        }
+
+        public double somar()
+        {
+            double resultado = 0;
+            foreach(double num in this.listNumero)
+            {
+                resultado += num;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+
+        public double subtrair()
+        {
+            double resultado = this.listNumero[0];
+            this.listNumero.RemoveAt(0);
+            foreach(double num in this.listNumero)
+            {
+                resultado -= num;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+
+        public double multiplicar()
+        {
+            double resultado = 1;
+            foreach (double numero in this.listNumero)
+            {
+                resultado *= numero;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+
+        public double dividir()
+        {
+            double resultado = this.listNumero[0];
+            foreach (double numero in this.listNumero)
+            {
+                resultado = this.listNumero[0] / numero;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+
+        public double media()
+        {
+            double resultado = 0;
+            double media = 0;
+            int cont = 0;
+
+            foreach (double numero in this.listNumero)
+            {
+                resultado += numero;
+                cont++;
+            }
+
+            media = resultado / cont;
+            return media;
+        }
+
+    }
+}
---
+++ ProgCalculadora/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc1 = new Calculadora();
+
+            calc1.addNumero(20.0);
+            calc1.addNumero(15.0);
+            System.Console.WriteLine(calc1.somar());
+
+            Calculadora calc2 = new Calculadora();
+
+            calc2.addNumero(20.0);
+            calc2.addNumero(10.0);
+            System.Console.WriteLine(calc2.subtrair());
+
+            Calculadora calc3 = new Calculadora();
+
+            calc3.addNumero(5.0);
+            calc3.addNumero(3.0);
+            System.Console.WriteLine(calc3.multiplicar());
+
+            Calculadora calc4 = new Calculadora();
+
+            calc4.addNumero(10);
+            calc4.addNumero(5);
+            System.Console.WriteLine(calc4.dividir());
+
+            Calculadora calc5 = new Calculadora();
+
+            calc5.addNumero(13);
+            calc5.addNumero(3);
+            calc5.addNumero(2);
+            System.Console.WriteLine(calc5.media());
+
+            System.Console.ReadKey();
+        }
+    }
+}
